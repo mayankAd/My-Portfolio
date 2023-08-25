fafd6cbd99cd01ff66c9f17bd4fb10de4fcf7fca
Mayank <97655679+mayankAd@users.noreply.github.com>
2023-08-25 15:49:38 +0530

    tech stack added

--- src/components/tech_stack/Tech_stack.jsx
+++ src/components/tech_stack/Tech_stack.jsx
@@ -2,7 +2,17 @@ import styles from './Tech_stack.module.css'
 
 const Tech_stack = () => {
   return (
-    <div className={`${styles.container}`}>Tech_stack</div>
+    <div className={`${styles.container}`}>
+      <h2>Tech Stack</h2>
+      <div className={`${styles.techs}`}>
+        <div className={`${styles.img_container}`}><img src="images/html.png" alt="" /></div>
+        <div className={`${styles.img_container}`}><img src="images/CSS.png" alt="" /></div>
+        <div className={`${styles.img_container}`}><img src="images/javascript.png" alt="" /></div>
+        <div className={`${styles.img_container}`}><img src="images/react4.png" alt="" /></div>
+        <div className={`${styles.img_container}`}><img src="images/mongodb3.png" alt="" /></div>
+        <div className={`${styles.img_container}`}><img src="images/tailwind2.png" alt="" /></div>
+      </div>
+    </div>
   )
 }
 
